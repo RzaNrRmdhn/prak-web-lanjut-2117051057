9af3ced06d9394ccdcf4792d25488837cc97c324
Reza Nur Ramadhan <rezanurramadhan292@gmail.com>
2023-09-14 02:57:55 +0700

    Routing dan Views Momen

---
+++ app/Views/profile.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <section style="font-family: Montserrat" class=" bg-[#071e34] flex font-medium items-center justify-center h-screen">
+        <section class="w-64 mx-auto bg-[#20354b] rounded-2xl px-8 py-6 shadow-lg">
+            <div class="flex items-center justify-between">
+                <span class="text-gray-400 text-sm">2d ago</span>
+                <span class="text-emerald-400">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z" />
+                    </svg>
+                </span>
+            </div>
+            <div class="mt-6 w-fit mx-auto">
+                <img src="<?php echo base_url('/assets/Mina.png'); ?>" class="rounded-full w-28 " alt="profile picture" srcset="">
+            </div>
+
+            <div class="mt-8 text-center">
+                <h2 class="text-white font-bold text-2l tracking-wide">Reza Nur Ramadhan</h2>
+            </div>
+            <div class="mt-2 text-center">
+                <h2 class="text-white font-bold text-2l tracking-wide">2117051057</h2>
+            </div>
+            <p class="text-center text-emerald-400 font-semibold mt-2.5" >
+                Kelas D
+            </p>
+
+            <div class="h-1 w-full bg-black mt-8 rounded-full">
+                <div class="h-1 rounded-full w-1/5 bg-yellow-500 "></div>
+            </div>
+            <div class="mt-3 text-white text-sm">
+                <span class="text-gray-400 font-semibold">Progress Life:</span>
+                <span>10%</span>
+            </div>
+        </section>
+    </section>
+</body>
+</html>
+
+
+<!-- component -->
+
+
